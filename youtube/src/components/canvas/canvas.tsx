a3ff49d4d980d6a01e85923b8120962beac4d69b
Maksim Kurdiumov <maksim_kurdiumov@senla.eu>
2021-02-16 17:16:23 +0300

    fixed

--- youtube/src/components/canvas/canvas.tsx
+++ youtube/src/components/canvas/canvas.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 import "./canvas.css";
 
@@ -9,66 +9,41 @@ type m = {
 
 const Canvas = () => {
 
-    const m: m = {
-        x: window.innerWidth / 2,
-        y: window.innerHeight / 2
-    };
-
-    window.onmousemove = function(e: any) {
-        m.x = e.clientX;
-        m.y = e.clientY;
-    }
+    const [x, setX] = useState(0),
+          [y, setY] = useState(0);
 
     useEffect(() => {
 
-        const drawEye = (x: number, y: number) => {
-            ctx!.beginPath();
-            ctx!.arc(x, y, 45, 0, Math.PI * 2, true);
-            ctx!.fillStyle = "#BFAE3F";
-            ctx!.fill();
-            ctx!.closePath();
-        };
-
-        // TODO rotate
-        const drawPupil = (x: number, y: number, angle: number) => {
-            ctx!.beginPath();
-            ctx!.arc(x, y, 20, 0, Math.PI * 2, true);
-            ctx!.fillStyle = "red";
-            ctx!.rotate(angle);
-            ctx!.fill();
-            ctx!.closePath();
-        };
-
-        // init
         const canvas = document.getElementById('canvas') as HTMLCanvasElement;
         const ctx = canvas.getContext('2d');
-        const frog = new Image();
-        frog.src = 'https://content.spiceworksstatic.com/service.community/p/post_images/0000342654/5c421b5f/attached_image/Hypnor.jpg';
 
-        // clear
+        window.onmousemove = function (e: any) {
+            setX(e.pageX);
+            setY(e.pageY);
+        }
+
         ctx!.clearRect(0, 0, canvas.width, canvas.height);
-        // update
-        ctx!.drawImage(frog, 0, 0);
-        drawEye(78, 54);
-        drawEye(225, 45);
-        drawPupil(78, 54, 0);
-        drawPupil(225, 45, 0);
 
-        // render
+        let a = canvas.getBoundingClientRect().left + 78;
+        let b = canvas.getBoundingClientRect().top + 54;
+
+        let radian = Math.atan2(x - a, y - b);
+        let angle = (radian * (180 / Math.PI) * -1) + 270;
+
+        ctx!.beginPath();
+        ctx!.rotate(angle);
+        ctx!.arc(
+            78, 
+            54, 
+            15, 
+            0, 
+            Math.PI * 2
+        );
+        ctx!.fillStyle = "red";
+        ctx!.fill();
+        ctx!.closePath();
 
     });
-
-        // const draw = () => {
-        //     const canvas = document.getElementById('tutorial') as HTMLCanvasElement;
-        //     const ctx = canvas.getContext('2d');
-
-        //     let x = (canvas.getBoundingClientRect().left) + (canvas.clientWidth / 2);
-        //     let y = (canvas.getBoundingClientRect().top) + (canvas.clientHeight / 2);
-
-        //     ctx!.clearRect(0, 0, canvas.width, canvas.height);
-
-        //     console.log(x + ' ' + y);
-        // }
 
     return (
         <div className="wrap mt-5">
